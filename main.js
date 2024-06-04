5dc7ae754c2e4139c4d7cdd143aeccb42712c4ea
luisMolina95 <luisrodmolina@gmail.com>
2024-06-04 11:43:19 -0600

    Add program

---
+++ main.js
@@ -0,0 +1,29 @@
+const {createServer} = require("http") ;
+
+const server = createServer((req, res) => {
+  const bodyChunk = [];
+  req.on("data", (chunk) => {
+    console.log("chunk:", chunk);
+    bodyChunk.push(chunk);
+  });
+  req.on("error", (a) => {
+    console.log("error:",a);
+  });
+  req.on("end", () => {
+    const stringBody = Buffer.concat(bodyChunk).toString();
+
+    res.writeHead(200, { "Content-Type": "application/json" });
+
+    res.end(
+      JSON.stringify({
+        data: "Hello World!",
+        headers: req.headers,
+        body: stringBody,
+        url: req.url,
+        method: req.method,
+      })
+    );
+  });
+});
+
+server.listen(process.env.PORT);
